80495f3c811f53d0b6b497ffd0a2916c2ece4b25
slipbrba <ibrahimbalyiyen28@gmail.com>
2022-10-26 00:20:09 +0300

    cs50 kutuphanesi eklendi

--- scores.c
+++ scores.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include "cs50.h"
 
 int main(){
-    printf("Hello world!");
+    printf("Hello world!\n");
+    int i = get_int("Enter an int: ");
+    printf("basardin, %d ", i);
 }
